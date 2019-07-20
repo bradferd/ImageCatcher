d16a4dd6da4b2318627d70b7d2f89320fc084521
Brad Bailey <bbailey921@gmail.com>
2019-07-19 22:56:50 -0400

    add controllers for collection model

--- controllers/collection.js
+++ controllers/collection.js
@@ -10,6 +10,49 @@ collectionRouter.get('/', async (req, res) => {
 	res.json(collections)
 })
 
+collectionRouter.get('/:collectionId', async (req, res) => {
+	try {
+		const collection = await collectionApi.getCollection(
+			req.params.collectionId
+		)
+		res.json(collection)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+collectionRouter.post('/', async (req, res) => {
+	try {
+		const newCollection = await collectionApi.addNewCollection(req.body)
+		res.json(newCollection)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+collectionRouter.put('/:collectionId', async (req, res) => {
+	try {
+		const updatedCollection = await collectionApi.updateCollection(
+			req.params.collectionId,
+			req.body
+		)
+		res.json(updatedCollection)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+collectionRouter.delete('/:collectionId', async (req, res) => {
+	try {
+		const deletedCollection = await collectionApi.deleteCollection(
+			req.params.collectionId
+		)
+		res.json(deletedCollection)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
 module.exports = {
 	collectionRouter
 }
