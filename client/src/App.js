13ffb96facd17043b2a8204361edeb1881a18118
Brad Bailey <bbailey921@gmail.com>
2019-07-20 09:46:58 -0400

    add semantic ui and navbar styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PhotoSearch from './components/ImageSearch/PhotoSearch.js'
+import Navbar from './components/Navbar'
 
 function App() {
 	return (
 		<div className='App'>
 			<Router>
+				<Navbar />
 				<Switch>
 					<Route exact path='/' />
 					<Route path='/photos' component={PhotoSearch} />
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Navbar extends Component {
 	render() {
-		return <nav />
+		return (
+			<div className='ui secondary pointing menu'>
+				<Link to='/' className='item'>
+					ImageCatcher
+				</Link>
+				<div className='right menu'>
+					<Link to='/photos' className='item'>
+						Image Search
+					</Link>
+					<Link to='/collections' className='item'>
+						All Collections
+					</Link>
+				</div>
+			</div>
+		)
 	}
 }
