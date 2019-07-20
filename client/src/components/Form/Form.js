5596ae1dfa6df89251cb5ee965a6b7c140279caa
Brad Bailey <bbailey921@gmail.com>
2019-07-20 09:36:25 -0400

    add components

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+	state = {
+		newCollection: {
+			name: '',
+			description: ''
+		}
+	}
+
+	handleInputChange = e => {
+		const copyCollection = { ...this.state.newCreature }
+		copyCollection[e.target.name] = e.target.value
+		this.setState({ newCollection: copyCollection })
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+		axios.post(`/api/collections`, this.state.newCollection)
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>{form.title}</h1>
-				<label>{form.name}</label>
-				<input />
-				<label>{form.description}</label>
-				<input />
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor='collection-name'>Collection Name</label>
+					<input
+						onChange={this.handleInputChange}
+						type='text'
+						id='collection-name'
+						placeholder='Enter a name for this collection...'
+						name='name'
+						value={this.state.newCollection.name}
+					/>
+					<label htmlFor='collection-description'>Collection Description</label>
+					<input
+						onChange={this.handleInputChange}
+						type='text'
+						id='collection-description'
+						name='description'
+						value={this.state.newCollection.description}
+					/>
+					<input type='submit' value='Create Collection' />
+				</form>
 			</div>
 		)
 	}
