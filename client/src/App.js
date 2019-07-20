bb6eee4bbcf44380c86155b5d658231d42bbac55
Brad Bailey <bbailey921@gmail.com>
2019-07-19 20:49:56 -0400

    add unsplash api and styling for images

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PhotoSearch from './components/PhotoSearch.js'
-import './App.css'
 
 function App() {
 	return (
--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ImageCard from './ImageCard'
+import '../ImageList.css'
 
 const ImageList = props => {
 	const images = props.images.map(image => {
