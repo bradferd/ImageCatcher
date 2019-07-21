05abae044949d391a90fa80392e8dc4c94c3dc72
Brad Bailey <bbailey921@gmail.com>
2019-07-21 09:47:21 -0400

    second model

---
+++ models/pictures.js
@@ -0,0 +1,31 @@
+const mongoose = require('./connection.js')
+
+const PictureSchema = new mongoose.Schema({
+	name: String,
+	description: String,
+	imgSrc: String,
+	collectionId: mongoose.Types.ObjectId
+})
+
+const PictureCollection = mongoose.model('Pictures', PicturesSchema)
+
+const getPicturesByCollection = collectionId =>
+	PictureCollection.find({ collectionId })
+
+const getPicture = pictureId => PictureCollection.findById(pictureId)
+
+const newPicture = pictureObject => PictureCollection.create(pictureObject)
+
+const editPicture = (pictureId, pictureObject) =>
+	PictureCollection.findByIdAndUpdate(pictureId, pictureObject)
+
+const deletePicture = pictureId =>
+	PictureCollection.findByIdAndDelete(pictureId)
+
+module.exports = {
+	getPicturesByCollection,
+	getPicture,
+	newPicture,
+	editPicture,
+	deletePicture
+}
