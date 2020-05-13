31ebae02b4b71b45113aa7d51b9f5be2e799aff2
bradferd <bbailey921@gmail.com>
2020-05-13 13:08:45 -0400

    refactor of navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default class Navbar extends Component {
-	state = {
-		isSignedIn: false,
-		email: null
-	}
-	signIn = googleUser => {
-		const profile = googleUser.getBasicProfile()
-		this.setState({ isSignedIn: true, email: profile.getEmail() })
-	}
-
-	signOut = async () => {
-		const auth2 = await window.gapi.auth2.getAuthInstance()
-		auth2.signOut()
-		console.log('user signed out')
-		this.setState({ isSignedIn: false })
-	}
-
-	render() {
-		return (
-			<div
-				className='ui inverted secondary pointing menu'
-				style={{ marginBottom: '0' }}
-			>
-				<Link to='/' className='item'>
-					<i className='camera retro inverted icon' /> ImageCatcher
+const Navbar = props => {
+	return (
+		<div
+			className='ui inverted secondary pointing menu'
+			style={{ marginBottom: '0' }}
+		>
+			<Link to='/' className='item'>
+				<i className='camera retro inverted icon' /> ImageCatcher
+			</Link>
+			<div className='right menu'>
+				<Link to='/collections/new' className='item'>
+					New Collection
 				</Link>
-				<div className='right menu'>
-					<Link to='/collections/new' className='item'>
-						New Collection
-					</Link>
-					<Link to='/collections' className='item'>
-						All Collections
-					</Link>
-				</div>
+				<Link to='/collections' className='item'>
+					All Collections
+				</Link>
 			</div>
-		)
-	}
+		</div>
+	)
 }
+
+export default Navbar
