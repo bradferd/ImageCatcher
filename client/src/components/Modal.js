236d84df47ac505ddef96d61f1fe1793361a8858
bradferd <bbailey921@gmail.com>
2020-05-13 13:15:40 -0400

    refactor Modal component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Modal = props => {
+const Modal = ({ onDismiss, title, actions, content }) => {
 	return ReactDOM.createPortal(
-		<div onClick={props.onDismiss} className='ui dimmer modals visible active'>
+		<div onClick={onDismiss} className='ui dimmer modals visible active'>
 			<div
 				onClick={e => e.stopPropagation()}
 				className='ui standard modal visible active'
 			>
-				{props.title ? <div className='header'>{props.title}</div> : null}
-				<div className='content'>{props.content}</div>
-				{props.actions ? <div className='actions'>{props.actions}</div> : null}
+				{title ? <div className='header'>{title}</div> : null}
+				<div className='content'>{content}</div>
+				{actions ? <div className='actions'>{actions}</div> : null}
 			</div>
 		</div>,
 		document.querySelector('#modal')
